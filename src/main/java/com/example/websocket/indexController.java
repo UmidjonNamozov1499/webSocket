1e7e7f057881362bccce245587ec4b48d8c75111
UmidMukhtorov <umidbekreal@gmail.com>
2024-10-07 23:36:17 +0500

    first-commit

---
+++ src/main/java/com/example/websocket/indexController.java
@@ -0,0 +1,12 @@
+package com.example.websocket;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class indexController {
+    @GetMapping
+    public String index(Model model){
+        return "index";
+    }
+}
